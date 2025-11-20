c2f0a58acb92be84ada62c5389a828060f83a2e9
gabstancu <gabstancu@outlook.com>
2025-11-20 13:46:48 +0200

    Add getters in LinearSystem.

--- include/LinearSystem.hpp
+++ include/LinearSystem.hpp
@@ -30,6 +30,16 @@ class LinearSystem
 
         void print_info ();
 
+        /* get */
+        SparseMatrix& A     () { return this->A_; }
+        Vector&       b     () { return this->b_; }
+        Vector&       u     () { return this->u_; }
+        double        l_min () { return this-> lambda_min; }
+        double        l_max () { return this-> lambda_max; }
+        double        kappa () { return this-> kappa_hat; }
+        int           n     () { return this->n_; }
+        int           m     () { return this->m_; }
+
     private:
         int n_ = 0; // number of variables
         int m_ = 0; // number of equations
@@ -38,9 +48,9 @@ class LinearSystem
         double lambda_max = 0.0;
         double kappa_hat  = 0.0;
 
-        SparseMatrix    A_; // system matrix
-        Eigen::VectorXd b_; // rhs vector
-        Eigen::VectorXd u_; // solution vector
+        SparseMatrix A_; // system matrix
+        Vector       b_; // rhs vector
+        Vector       u_; // solution vector
 
 };
 
--- include/solve/CG.hpp
+++ include/solve/CG.hpp
@@ -1,7 +1,29 @@
 #ifndef CG_HPP
 #define CG_HPP
 
+#include "Eigen/Eigen"
 
+using SparseMatrix = Eigen::SparseMatrix<double, Eigen::RowMajor>;
+using DenseMatrix  = Eigen::MatrixXd;
+using Vector       = Eigen::VectorXd;
 
+class ConjugateGradient
+{
+    private:
+        double      tol       = 1e-6;
+        int         max_iters = 1e6;
+        std::string name      = "CG";
+        Vector      u_;
+
+    public:
+        ConjugateGradient () = default;
+        ConjugateGradient (double tolerance, int max_iterations);
+
+        template<class System>
+        void solve (System& system)
+        {
+            
+        }
+};
 
 #endif // CG_HPP
